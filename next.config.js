6fe74b24bc379b89da54c307a33169223ddc85db
soundwanders <soundwanders@gmail.com>
2023-10-12 18:59:10 -0400

    Prettier + prettier-tailwind-css plugin

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const path = require("path");
 const nextConfig = {
   serverRuntimeConfig: {
     LOCAL_URL: "http://localhost:3000",
-    NEXT_PUBLIC_VERCEL_URL: "https://toraneko.vercel.app"
+    NEXT_PUBLIC_VERCEL_URL: "https://toraneko.vercel.app",
   },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
